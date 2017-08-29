199c0fdccfe7e3e3d50c444e9360a99c49a6c6ed
TONG XIAO XIAN <tongxiaoxian@icloud.com>
2017-08-29 20:07:24 +0800

    no message

--- msm/leavebehindtheworld.com/libraries/js/intro.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $("body").each(function(index) {
-        var image = $(this).children("img#backstretch").attr("src");
-        $(this).backstretch(image);
-    });
-});
--- msm/leavebehindtheworld.com/libraries/js/prevent.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    document.oncontextmenu = function () { return false; }
-});
-
-$(document).ready(function() {
-	$('img').css({pointerEvents: "none"})
-
-});
-

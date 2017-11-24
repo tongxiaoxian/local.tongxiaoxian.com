5ae1867e397ecdb9dc63be8a5e00885323c5084d
TONG XIAO XIAN <tongxiaoxian@icloud.com>
2017-11-24 15:28:17 +0800

    no message

--- public_html/libraries/js/baidu.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?f636f3cb23533ea55dd8006c06d3c409";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
